4816fd23cf5105b59bb547b0194fc576a5a40640
Jordane Pelloux-Prayer <jordane@damsy.net>
2017-05-26 19:11:07 +0200

    feat: try to not overwrite the global geojson
    try to open the global geojson file and change only the modified city

--- tools/osm-cadastre.py
+++ tools/osm-cadastre.py
@@ -214,10 +214,25 @@ def build_municipality_list(department, vectorized, insee=None, force_download=F
     # write geojson
     log.debug('Write {}.geojson'.format(department))
     geojson_path = path.join(STATS_PATH, '{}.geojson'.format(department))
+    if path.exists( geojson_path ) and insee:
+        department_geojson = geojson.loads( open( geojson_path ).read() )
+        found = False
+        for municipality in department_geojson["features"]:
+            if municipality["properties"]["insee"] == insee:
+                found = True
+                index = department_geojson["features"].index( municipality )
+                department_geojson["features"] = department_geojson["features"][:index] + department_stats + department_geojson["features"][index+1:]
+                break
+        if not found:
+            department_geojson["features"] += department_stats
+
+    else:
+        department_geojson = FeatureCollection(department_stats)
+        
     with open(geojson_path, 'w') as fd:
         # we should not indent the GeoJSON because it drastically reduce the final size
         # (x10 or so)
-        fd.write(geojson.dumps(FeatureCollection(department_stats), indent=None))
+        fd.write(geojson.dumps(department_geojson, indent=None))
 
     # write txt
     log.debug('Write {}-municipality.txt'.format(department))
