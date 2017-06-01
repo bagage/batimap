24498460b2e4112488cc06bd4294a78e15722bbc
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2017-06-01 12:48:50 +0200

    style: apply flake8 rules

--- website/app.py
+++ website/app.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 db = Postgis('gis', 'docker', 'docker', 25432, 'localhost')
 
+
 @app.route('/')
 def index() -> object:
     return render_template('index.html')
@@ -16,14 +17,17 @@ def api_insee(insee) -> dict:
     color_city = db.get_insee(insee)
     return geojson.dumps(color_city)
 
+
 @app.route('/colors/<lonNW>/<latNW>/<lonSE>/<latSE>/<colors>', methods=['GET'])
 def api_color(colors, lonNW, latNW, lonSE, latSE) -> dict:
     color_city = db.get_city_with_colors(colors.split(','), float(lonNW), float(latNW), float(lonSE), float(latSE))
     return geojson.dumps(color_city)
 
+
 @app.route('/colors', methods=['GET'])
 def api_colors_list() -> dict:
     return jsonify(db.get_colors())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- website/db_utils.py
+++ website/db_utils.py
@@ -1,8 +1,8 @@
 import psycopg2
-import json
 
 from geojson import Feature, FeatureCollection, loads
 from math import sqrt
+
 
 class Postgis(object):
     def __init__(self, db, user, passw, port, host):
@@ -11,11 +11,11 @@ class Postgis(object):
 
     def get_insee(self, insee: int) -> FeatureCollection:
         req = ((""
-                            "        SELECT p.name, c.insee, c.color, ST_AsGeoJSON(p.way) AS geometry\n"
-                            "        FROM planet_osm_polygon p, color_city c\n"
-                            "        WHERE p.tags->'ref:INSEE' = '{}'\n"
-                            "        AND p.tags->'ref:INSEE' = c.insee\n"
-                            "").format(insee))
+                "        SELECT p.name, c.insee, c.color, ST_AsGeoJSON(p.way) AS geometry\n"
+                "        FROM planet_osm_polygon p, color_city c\n"
+                "        WHERE p.tags->'ref:INSEE' = '{}'\n"
+                "        AND p.tags->'ref:INSEE' = c.insee\n"
+                "").format(insee))
         self.cursor.execute(req)
         features = []
 
@@ -30,33 +30,31 @@ class Postgis(object):
 
     def get_colors(self):
         req = ((""
-                            "        SELECT DISTINCT color\n"
-                            "        FROM color_city\n"
-                            ""))
+                "        SELECT DISTINCT color\n"
+                "        FROM color_city\n"
+                ""))
         self.cursor.execute(req)
-        features = []
 
         return sorted([x[0].strip() for x in self.cursor.fetchall()])
 
-
     def get_city_with_colors(self, colors, lonNW: float, latNW: float, lonSE: float, latSE: float) -> FeatureCollection:
         req = ((""
-                            "        SELECT count(p.name)\n"
-                            "        FROM planet_osm_polygon p, color_city c\n"
-                            "        WHERE p.tags->'admin_level' = '8'\n"
-                            "        AND c.insee = p.tags->'ref:INSEE'\n"
-                            "        AND c.color in ('{colors}')\n"
-                            "").format(colors="','".join(colors)))
+                "        SELECT count(p.name)\n"
+                "        FROM planet_osm_polygon p, color_city c\n"
+                "        WHERE p.tags->'admin_level' = '8'\n"
+                "        AND c.insee = p.tags->'ref:INSEE'\n"
+                "        AND c.color in ('{colors}')\n"
+                "").format(colors="','".join(colors)))
         self.cursor.execute(req)
         count = self.cursor.fetchall()[0][0]
 
         req = (""
-                            "        SELECT p.name, c.insee, c.color, ST_AsGeoJSON(p.way, 6) AS geometry\n"
-                            "        FROM planet_osm_polygon p, color_city c\n"
-                            "        WHERE p.tags->'admin_level' = '8'\n"
-                            "        AND c.insee = p.tags->'ref:INSEE'\n"
-                            "        AND c.color in ('{colors}')\n"
-                            "").format(colors="','".join(colors))
+               "        SELECT p.name, c.insee, c.color, ST_AsGeoJSON(p.way, 6) AS geometry\n"
+               "        FROM planet_osm_polygon p, color_city c\n"
+               "        WHERE p.tags->'admin_level' = '8'\n"
+               "        AND c.insee = p.tags->'ref:INSEE'\n"
+               "        AND c.color in ('{colors}')\n"
+               "").format(colors="','".join(colors))
 
         # if there are too much cities, filter by distance
         if len(colors) > 1 and count > 1500:
@@ -66,9 +64,9 @@ class Postgis(object):
             maxDistance = min(1., maxDistance)
 
             req += ((""
-                                "        AND ST_DWithin(way, ST_SetSRID(ST_MakePoint({lon}, {lat}),4326), {distance})\n"
-                                "        ORDER BY ST_Distance(ST_SetSRID(ST_MakePoint({lon}, {lat}),4326), p.way)\n"
-                                "").format(lon=(lonNW + lonSE) / 2., lat=(latNW + latSE) / 2., distance=maxDistance))
+                     "        AND ST_DWithin(way, ST_SetSRID(ST_MakePoint({lon}, {lat}),4326), {distance})\n"
+                     "        ORDER BY ST_Distance(ST_SetSRID(ST_MakePoint({lon}, {lat}),4326), p.way)\n"
+                     "").format(lon=(lonNW + lonSE) / 2., lat=(latNW + latSE) / 2., distance=maxDistance))
 
         self.cursor.execute(req)
 
