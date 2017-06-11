6bed17bb5a602d3d3429b55328a0f511007a97cb
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2017-06-11 20:50:26 +0200

    feat(html): add Hash plugin to have z/x/y in URL

--- website/static/js/map.js
+++ website/static/js/map.js
@@ -14,6 +14,7 @@ $(function () {
         if (!map.restoreView()) {
             map.setView(initPosition, 6);
         }
+        new L.Hash(map); // display url with z/x/y params
 
         bgLayer = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png',
             { attribution: '© Contributeurs OpenStreetMap'}
