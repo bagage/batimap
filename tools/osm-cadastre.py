55d28fbc1e56d642d76a273025b73c5c676e2be2
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2017-07-08 23:00:07 +0200

    fix: add min/max zoom for Strava layer

--- tools/osm-cadastre.py
+++ tools/osm-cadastre.py
@@ -768,13 +768,13 @@ def work(args):
     # b. open Strava and BDOrtho IGN imageries
     imageries = OrderedDict([
         ("BDOrtho IGN",
-         "http://proxy-ign.openstreetmap.fr/bdortho/{zoom}/{x}/{y}.jpg"),
+         "url=http://proxy-ign.openstreetmap.fr/bdortho/{zoom}/{x}/{y}.jpg"),
         ("Strava",
-         "http://globalheat.strava.com/tiles/both/color2/{zoom}/{x}/{y}.png"),
+         "min_zoom=3&max_zoom=17&url=http://globalheat.strava.com/tiles/both/color2/{zoom}/{x}/{y}.png"),
     ])
     for k, v in imageries.items():
         r = requests.get(
-            base_url + 'imagery?title={}&type=tms&url={}'.format(k, v))
+            base_url + 'imagery?title={}&type=tms&{}'.format(k, v))
         if r.status_code != 200:
             log.critical("Cannot add imagery ({}): {}".format(
                 r.status_code, r.text))
