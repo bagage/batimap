a6f2527d5cffa5593b333a94c92a1350ca5f260f
Rodolphe Pelloux-Prayer <rodolphe@damsy.net>
2017-07-26 15:07:35 +0200

    refactor: move all cli from batimap to flask.cli
    Signed-off-by: Rodolphe Pelloux-Prayer <rodolphe@damsy.net>

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
+import click
 import geojson
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 from batimap import batimap
-from db_utils import Postgis
 from batimap.overpassw import Overpass
+from db_utils import Postgis
 
 app = Flask(__name__)
 CORS(app)
@@ -20,6 +21,7 @@ db = Postgis(app.config['DB_NAME'],
 op = Overpass(app.config['OVERPASS_URI'])
 
 
+# ROUTES
 @app.route('/insee/<int:insee>', methods=['GET'])
 def api_insee(insee) -> dict:
     color_city = db.get_insee(insee)
@@ -54,5 +56,33 @@ def update_insee_list(insee) -> dict:
     return "Ok"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+# CLI
+@app.cli.command()
+@click.argument('city')
+def update_city_stats(city):
+    batimap.stats(db, op, cities=[city])
+
+
+@app.cli.command()
+@click.argument('department')
+def update_department_stats(department):
+    batimap.stats(db, op, department=department)
+
+
+@app.cli.command()
+def update_france_stats():
+    for r in (range(1, 20), ('2A', '2B'), range(21, 96), range(971, 977)):
+        for d in r:
+            batimap.stats(db, op, department=str(d))
+
+
+@app.cli.command()
+@click.argument('city')
+def generate_city_building(city):
+    batimap.generate(db, cities=[city])
+
+
+@app.cli.command()
+@click.argument('city')
+def load_city_josm(city):
+    batimap.work(cities=[city])
--- backend/batimap/batimap.py
+++ backend/batimap/batimap.py
@@ -1,38 +1,23 @@
 #!/usr/bin/env python3
-import argparse
-import configparser
 import datetime
 import logging
 import shutil
-import sys
 from os import path
-
-import argcomplete
-from colorlog import ColoredFormatter
 
 from .city import City
 from .josm import Josm
 
 
-LEVELS = {
-    'no': logging.CRITICAL,
-    'error': logging.ERROR,
-    'warning': logging.WARNING,
-    'info': logging.INFO,
-    'debug': logging.DEBUG,
-}
-LOG = logging.getLogger('batimap')
+LOG = logging.getLogger(__name__)
 
 
 def stats(db, overpass, department=None, cities=[], force=False):
     if department:
-        cities = db.within_departments(
-            [d.zfill(2) for d in department])
+        cities = db.within_department(department)
 
     for city in cities:
         c = City(db, city)
-        print(c)
-        (date, author) = c.fetch_osm_data(overpass, force)
+        c.fetch_osm_data(overpass, force)
 
 
 def generate(db, cities):
@@ -66,121 +51,3 @@ def work(db, cities):
                 "Déplacement de {} vers les archives".format(city_path))
             shutil.move(city_path, path.join(
                 City.WORKDONE_PATH, path.basename(city_path)))
-
-
-def configure_logging(verbosity):
-    log_level = LEVELS[verbosity] if verbosity in LEVELS else logging.WARNING
-
-    if sys.stdout.isatty():
-        formatter = ColoredFormatter(
-            '%(asctime)s %(log_color)s%(message)s%(reset)s', "%H:%M:%S")
-        stream = logging.StreamHandler()
-        stream.setFormatter(formatter)
-        LOG.addHandler(stream)
-    else:
-        logging.basicConfig(
-            format='%(asctime)s %(message)s', datefmt="%H:%M:%S")
-    LOG.setLevel(log_level)
-
-
-def batimap():
-    parser = argparse.ArgumentParser(
-        description="Inspection de l'état du bâti dans OpenStreetMap en France."
-    )
-    parser.add_argument(
-        "-c", "--config",
-        dest='config_file',
-        default='config.ini',
-        type=str,
-        help="Fichier de configuration",
-    )
-    parser.add_argument(
-        '-v', '--verbose',
-        choices=LEVELS.keys(),
-        help="Niveau de verbosité",
-    )
-    parser.add_argument(
-        '--database',
-        type=str,
-        help="Identifiants pour la base de donnée (host:port:user:password:database)",
-        required=True,
-    )
-
-    subparsers = parser.add_subparsers(
-        help="Plusieurs commandes sont disponibles"
-    )
-    subparsers.required = True
-    subparsers.dest = 'command'
-
-    stats_parser = subparsers.add_parser(
-        'stats',
-        help="Récupère la date du dernier import du bâti pour une commune ou département",
-    )
-    stats_parser.add_argument(
-        '-f', '--force',
-        action='store_true',
-        help="Ne pas utiliser la valeur en base de donnée",
-    )
-    stats_group = stats_parser.add_mutually_exclusive_group(required=True)
-    stats_group.add_argument(
-        '-d', '--department',
-        type=str,
-        nargs='+',
-        help="département par son numéro",
-    )
-    stats_group.add_argument(
-        '-c', '--cities',
-        type=str,
-        nargs='+',
-        help="communes par numéro INSEE ou nom",
-    )
-    stats_group.set_defaults(func=stats)
-
-    generate_parser = subparsers.add_parser(
-        'generate',
-        help="Génère le bâti depuis le cadastre",
-    )
-    generate_group = generate_parser.add_mutually_exclusive_group(
-        required=True,
-    )
-    generate_group.add_argument(
-        '-c', '--cities',
-        type=str,
-        nargs='+',
-        help="communes par numéro INSEE ou nom",
-    )
-    generate_parser.set_defaults(func=generate)
-
-    work_parser = subparsers.add_parser(
-        'work',
-        help="Met en place JOSM pour effectuer le travail de mise à jour du bâti",
-    )
-    work_parser.add_argument(
-        '-c', '--cities',
-        type=str,
-        nargs='+',
-        help="communes par numéro INSEE ou nom",
-        required=True,
-    )
-    work_parser.set_defaults(func=work)
-
-    argcomplete.autocomplete(parser)
-    args = parser.parse_args()
-
-    config = configparser.ConfigParser(
-        defaults={
-            'here': path.realpath('.'),
-        }
-    )
-    config.read(args.config_file)
-
-    configure_logging(args.verbose)
-
-    try:
-        args.func(args)
-    except KeyboardInterrupt as e:
-        pass
-
-
-if __name__ == '__main__':
-    batimap()
