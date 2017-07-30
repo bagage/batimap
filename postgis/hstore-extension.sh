15ff277694b96ed05f204ae6df7e752298549518
Rodolphe Pelloux-Prayer <rodolphe@damsy.net>
2017-07-30 21:15:46 +0200

    refactor: configure docker-compose for dev
    Signed-off-by: Rodolphe Pelloux-Prayer <rodolphe@damsy.net>

---
+++ postgis/hstore-extension.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Perform all actions as $POSTGRES_USER
+export PGUSER="$POSTGRES_USER"
+
+# Load hstore into both template_database and $POSTGRES_DB
+for DB in template_postgis "$POSTGRES_DB"; do
+	echo "Loading PostGIS extensions into $DB"
+	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
+		CREATE EXTENSION IF NOT EXISTS hstore;
+EOSQL
+done
