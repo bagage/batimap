9714192bad6a8237f288b590b8187acd2b5a128c
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2018-07-11 20:07:12 +0200

    chore: use docker-compose config by default

--- postgis/hstore-extension.sh
+++ postgis/hstore-extension.sh
@@ -7,7 +7,7 @@ export PGUSER="$POSTGRES_USER"
 # Load hstore into both template_database and $POSTGRES_DB
 for DB in template_postgis "$POSTGRES_DB"; do
 	echo "Loading PostGIS extensions into $DB"
-	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
+	psql --dbname="$DB" <<-'EOSQL'
 		CREATE EXTENSION IF NOT EXISTS hstore;
 EOSQL
 done
