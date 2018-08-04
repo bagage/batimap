00e475b3c15cc4eecefd1003d484def1bfc57a4d
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2018-08-04 20:07:17 +0200

    feat: do not apply wall=yes if no conflation is needed

--- josm-scripts/2conflation.js
+++ josm-scripts/2conflation.js
@@ -14,18 +14,7 @@ for (var i = 0; i < josm.layers.length; i++) {
 
 
 function do_work()  {
-    // 2. Cadastre layer: add wall=yes to everything that has not wall=no and select buildings
-    var command = require("josm/command");
-    var ds1 = housesLayer.data
-    var walls = ds1.query('-wall=no -child');
-    command.change(walls, {
-       tags: {"wall": "yes"}
-    }).applyTo(housesLayer);
-    var buildings = ds1.query('building=* -type:relation');
-    ds1.selection.clearAll();
-    ds1.selection.add(buildings);
-
-    // 3. OSM layer: select all elements within relation by first selecting city, then all inside, then filter buildings
+    // 2. OSM layer: select all elements within relation by first selecting city, then all inside, then filter buildings
     insee = osmLayer.name.split(" ").splice(-3)[0];
     var ds2 = osmLayer.data;
     var city = ds2.query('"ref:INSEE"=' + insee);
@@ -45,6 +34,22 @@ function do_work()  {
     var buildings = ds2.query('selected building=* -type:relation');
     ds2.selection.clearAll();
     ds2.selection.add(buildings);
+
+    if (ds2.selectionEmpty()) {
+        josm.alert("Il ne semble y avoir aucun bâtiment actuellement dans cette ville. Aucune conflation à réaliser, il suffit d'importer les bâtiments du calque " + housesLayer.name);
+        return;
+    }
+
+    // 1. Cadastre layer: add wall=yes to everything that has not wall=no and select buildings
+    var command = require("josm/command");
+    var ds1 = housesLayer.data
+    var walls = ds1.query('-wall=no -child');
+    command.change(walls, {
+       tags: {"wall": "yes"}
+    }).applyTo(housesLayer);
+    var buildings = ds1.query('building=* -type:relation');
+    ds1.selection.clearAll();
+    ds1.selection.add(buildings);
 
     // 4. select work layer
     josm.layers.activeLayer = osmLayer;
