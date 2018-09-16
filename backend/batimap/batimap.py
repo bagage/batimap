9020ee31784fd214c1e713fa058bd893003916bd
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2018-09-16 22:23:58 +0200

    fix(backend): do not put null values in city_stats because postgre cannot group by null values

--- backend/batimap/batimap.py
+++ backend/batimap/batimap.py
@@ -71,7 +71,7 @@ def update_departments_raster_state(db, departments):
                 LOG.critical(f"Cannot find city with insee {insee}, did you import OSM data for this department?")
                 continue
 
-            date = 'raster' if is_raster else None
+            date = 'raster' if is_raster else 'never'
             tuples.append((insee, dept, name, f"{code_commune}-{nom_commune}", is_raster, date))
         db.insert_stats_for_insee(tuples)
 
