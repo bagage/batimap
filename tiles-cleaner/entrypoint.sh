a95dd56d11dc4c53165ad497515c5f6cdf22cf84
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2018-10-18 20:50:55 +0200

    feat(tiles): properly cache tiles

--- tiles-cleaner/entrypoint.sh
+++ tiles-cleaner/entrypoint.sh
@@ -1,15 +1,22 @@
-#!/bin/sh
+#!/bin/sh -x
 
 INPUT_FILE=/app/data/outdated/outdated.txt
 WORK_FILE=/app/data/outdated/inprogress.txt
 
 cd /app
+
+# cache whole world at start, if needed
+rm -rf data/cache/*
+tegola cache seed --max-zoom 8
+
 while true; do
     if [ -s $INPUT_FILE ]; then
         echo "There is $(wc -l $INPUT_FILE) outdated tiles to treat..."
         cat $INPUT_FILE | sort | uniq > $WORK_FILE
-        tegola cache purge --tile-list $WORK_FILE --max-zoom 8
-        tegola cache seed --tile-list $WORK_FILE --max-zoom 8
+        while read bbox; do
+            tegola cache purge --bounds "$bbox" --min-zoom 8 --max-zoom 13
+            tegola cache seed --bounds "$bbox" --min-zoom 8 --max-zoom 10
+        done < $WORK_FILE
         rm $WORK_FILE
         echo -n > $INPUT_FILE
         echo "All tiles treated!"
