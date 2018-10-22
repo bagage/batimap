ca27a9bf17fbfb2d17a62265b49b053e0ec5ea54
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2018-10-22 20:39:10 +0200

    fix: add default value for POSTGRES_PORT if not set

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -x
 
+POSTGRES_PORT=${POSTGRES_PORT:-5432}
+
 count=$(PGPASSWORD=$POSTGRES_PASSWORD psql -qtA -U $POSTGRES_USER -h $POSTGRES_HOST -p $POSTGRES_PORT -d $POSTGRES_DB -c "SELECT count(*) FROM city_stats where date like '20%'")
 result=$?
 
