6b24cb18affeb0e899a3c42606ef77d089266af9
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2018-10-22 23:34:02 +0200

    fix(backend): properly parse boundingbox when it contains 3e-5 notation

--- backend/batimap/bbox.py
+++ backend/batimap/bbox.py
@@ -3,15 +3,17 @@ import re
 
 class Bbox(object):
 
-    str_regex = re.compile(
-        r'BOX\((-{0,1}[\d\.]+) (-{0,1}[\d\.]+),(-{0,1}[\d\.]+) (-{0,1}[\d\.]+)\)')
+    # cf https://docs.python.org/3/library/re.html#simulating-scanf
+    # need to handle 10e3 notation too
+    float_re = r'([-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)'
+    box_re = re.compile(r'BOX\(' + float_re + ' ' + float_re + ',' + float_re + ' ' + float_re + r'\)')
 
     def __init__(self, from_string):
-        groups = self.str_regex.match(from_string).groups()
+        groups = self.box_re.match(from_string).groups()
         self.xmin = float(groups[0])
-        self.ymin = float(groups[1])
-        self.xmax = float(groups[2])
-        self.ymax = float(groups[3])
+        self.ymin = float(groups[4])
+        self.xmax = float(groups[8])
+        self.ymax = float(groups[12])
 
     def __repr__(self):
         return f'{self.xmin},{self.ymin},{self.xmax},{self.ymax}'
