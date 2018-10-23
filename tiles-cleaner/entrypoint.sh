462e256113b7d66c15d3a51a2e0123a13de27c98
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2018-10-23 20:53:15 +0200

    fix(tiles-cleaner): avoid cleaning tiles multiple times

--- tiles-cleaner/entrypoint.sh
+++ tiles-cleaner/entrypoint.sh
@@ -8,17 +8,18 @@ cd /app
 # cache whole world at start, if needed
 rm -rf data/cache/*
 tegola cache seed --max-zoom 10
+echo > $INPUT_FILE
 
 while true; do
     if [ -s $INPUT_FILE ]; then
         echo "There is $(wc -l $INPUT_FILE) outdated tiles to treat..."
         cat $INPUT_FILE | sort | uniq > $WORK_FILE
+        echo -n > $INPUT_FILE
         while read bbox; do
             tegola cache purge --bounds "$bbox" --min-zoom 8 --max-zoom 13
             tegola cache seed --bounds "$bbox" --min-zoom 8 --max-zoom 10
         done < $WORK_FILE
         rm $WORK_FILE
-        echo -n > $INPUT_FILE
         echo "All tiles treated!"
     fi
     sleep 30
