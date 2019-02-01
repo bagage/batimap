e4117cc9768d5e5c8e521e2a0dc98c6aadc233dc
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2019-02-01 18:21:58 +0100

    feat(initdb_worker): use info log

--- initdb_worker/app.py
+++ initdb_worker/app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import sys
 import requests
 from math import degrees, sinh, atan, pi
 import configparser
@@ -26,25 +25,25 @@ verbosity = {
 logging.basicConfig(
     format="%(asctime)s %(message)s",
     datefmt="%H:%M:%S",
-    level=verbosity[
-        os.environ.get("INITDB_VERBOSITY")
-        or config["DEFAULT"]["VERBOSITY"]
-        or "CRITICAL"
-    ],
+    level=verbosity[os.environ.get("INITDB_VERBOSITY") or config["DEFAULT"]["VERBOSITY"] or "CRITICAL"],
 )
 
-BACKEND_DEPARTMENTS_IN_BBOX_URL=config["DEFAULT"]["BACKEND_URL"] + '/departments/in_bbox/{lonNW}/{latNW}/{lonSE}/{latSE}'
-BACKEND_INITDB_URL=config["DEFAULT"]["BACKEND_URL"] + '/initdb?{}'
-BACKEND_TASKS_URL=config["DEFAULT"]["BACKEND_URL"] + '/tasks/{task_id}'
+BACKEND_DEPARTMENTS_IN_BBOX_URL = (
+    config["DEFAULT"]["BACKEND_URL"] + "/departments/in_bbox/{lonNW}/{latNW}/{lonSE}/{latSE}"
+)
+BACKEND_INITDB_URL = config["DEFAULT"]["BACKEND_URL"] + "/initdb?{}"
+BACKEND_TASKS_URL = config["DEFAULT"]["BACKEND_URL"] + "/tasks/{task_id}"
+
 
 def convert_zxy_to_lonlat(z, x, y):
     n = pow(2, z)
     lat = x / n * 360.0 - 180.0
-    lon = degrees( atan( sinh( pi * (1 - (2 * y / n)))))
+    lon = degrees(atan(sinh(pi * (1 - (2 * y / n)))))
     return [lon, lat]
 
+
 class Watcher:
-    DIRECTORY_TO_WATCH="/tiles"
+    DIRECTORY_TO_WATCH = "/tiles"
 
     def __init__(self):
         self.observer = Observer()
@@ -61,31 +60,33 @@ class Watcher:
             self.observer.stop()
         self.observer.join()
 
+
 class Handler(FileSystemEventHandler):
     @staticmethod
     def on_any_event(event):
         if event.is_directory:
             return None
-        elif event.event_type == 'modified':
-            LOG.debug( f"New entries in {event.src_path}")
+        elif event.event_type == "modified":
+            LOG.debug(f"New entries in {event.src_path}")
             lines = None
             with open(event.src_path) as f:
                 lines = f.readlines()
             departments = []
             for i, line in enumerate(lines):
-                LOG.debug( f"entry {i} / {len(lines)} : {line}")
-                z, x, y = map(int, line.split('/'))
+                LOG.debug(f"entry {i} / {len(lines)} : {line}")
+                z, x, y = map(int, line.split("/"))
                 lonNW, latNW = convert_zxy_to_lonlat(z, x, y)
-                lonSE, latSE = convert_zxy_to_lonlat(z, x+1, y+1)
+                lonSE, latSE = convert_zxy_to_lonlat(z, x + 1, y + 1)
                 args = {"lonNW": lonNW, "latNW": latNW, "lonSE": lonSE, "latSE": latSE}
                 LOG.debug(BACKEND_DEPARTMENTS_IN_BBOX_URL.format(**args))
-                r = requests.get( url=BACKEND_DEPARTMENTS_IN_BBOX_URL.format(**args) )
+                r = requests.get(url=BACKEND_DEPARTMENTS_IN_BBOX_URL.format(**args))
                 departments += r.json()
                 LOG.debug(r.text)
             departments = list(set(departments))
-            LOG.debug(f"Running initdb on {departments}")
-            r = requests.post(url=BACKEND_INITDB_URL.format('&'.join(departments)))
+            LOG.info(f"Running initdb on {departments}")
+            r = requests.post(url=BACKEND_INITDB_URL.format("&".join(departments)))
             LOG.debug(f"You can follow the progress of initdb on {BACKEND_TASKS_URL.format(**r.json())}")
+
 
 if __name__ == "__main__":
     w = Watcher()
