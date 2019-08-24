8c9be5b230a0ddf396241b54991da6e1825fdd61
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2019-08-24 13:51:27 +0200

    fix(front): hide legend if zoom < 8

--- front/src/app/pages/map/map.module.ts
+++ front/src/app/pages/map/map.module.ts
@@ -4,10 +4,10 @@ import { CitiesListModule } from '../cities-list/cities-list.module';
 import { MapComponent } from './map.component';
 
 import { CommonModule } from '@angular/common';
+import '@bagage/leaflet.restoreview';
 import 'leaflet';
 import 'leaflet-geocoder-ban/dist/leaflet-geocoder-ban';
 import 'leaflet-hash';
-import '@bagage/leaflet.restoreview';
 import 'leaflet.vectorgrid';
 import { SharedComponentsModule } from '../../components/shared-components.module';
 import { MatLibModule } from '../../mat-lib.module';
