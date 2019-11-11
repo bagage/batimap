9be6db7837c924862a8a5f7caac59fe5eead7388
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2019-11-11 17:28:21 +0100

    fix(tiles-cleaner): ensure that min-zoom is always lower than max-zoom

--- tiles-cleaner/entrypoint.sh
+++ tiles-cleaner/entrypoint.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 INIT_FILE="/data/initdb_is_done"
 CACHE_DIR="/data/cache"
 UPDATE_FILE=/data/outdated.txt
 WORK_FILE=/data/inprogress.txt
 INITIAL_MAX_ZOOM=${INITIAL_MAX_ZOOM:-10}
+MIN_ZOOM=7
+if [ $INITIAL_MAX_ZOOM -lt $MIN_ZOOM ]; then
+    MIN_ZOOM=$((INITIAL_MAX_ZOOM - 1))
+fi
 
 cd /app
 
@@ -26,9 +30,9 @@ while true; do
                 "44.7437,-13.2733,45.507,-12.379" # 976 Mayotte
             )
             for bbox in "${france_bboxes[@]}"; do
-                echo "Regenrating tiles in bbox $bbox"
-                tegola --config /app/config.toml cache purge --bounds "$bbox" --min-zoom 7 --max-zoom 13
-                tegola --config /app/config.toml cache seed --bounds "$bbox" --min-zoom 7 --max-zoom $INITIAL_MAX_ZOOM
+                echo "Regenerating tiles in bbox $bbox"
+                tegola --config /app/config.toml cache purge --bounds "$bbox" --min-zoom $MIN_ZOOM --max-zoom 13
+                tegola --config /app/config.toml cache seed --bounds "$bbox" --min-zoom $MIN_ZOOM --max-zoom $INITIAL_MAX_ZOOM
             done
         else
             echo "Removing all tiles, will regenerate up to level $INITIAL_MAX_ZOOM!"
@@ -47,8 +51,8 @@ while true; do
             # if bbox not empty
             if [ ! -z $bbox ]; then
                 echo "Regenrating tiles in bbox $bbox"
-                tegola --config /app/config.toml cache purge --bounds "$bbox" --min-zoom 7 --max-zoom 13
-                tegola --config /app/config.toml cache seed --bounds "$bbox" --min-zoom 7 --max-zoom $INITIAL_MAX_ZOOM
+                tegola --config /app/config.toml cache purge --bounds "$bbox" --min-zoom $MIN_ZOOM --max-zoom 13
+                tegola --config /app/config.toml cache seed --bounds "$bbox" --min-zoom $MIN_ZOOM --max-zoom $INITIAL_MAX_ZOOM
             fi
         done < $WORK_FILE
         rm $WORK_FILE
