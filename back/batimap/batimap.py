ff41be969c9db225e63be9eafaef2a1885d0a8c0
Gautier P <gautier+git@damsy.net>
2020-08-07 13:34:19 +0200

    feat(back): ignore small city

--- back/batimap/batimap.py
+++ back/batimap/batimap.py
@@ -23,6 +23,7 @@ class Batimap(object):
     IGNORED_SIMPLIFIED_TAGS = ["power", "public_transport", "ruins", "telecom", "historic", "ref:mhs", "tower:type"]
     IGNORED_SIMPLIFIED_BUILDING_VALUES = ["hut", "shed", "no", "ruins", "bunker", "wayside_shrine", "hunting_stand", "transformation_tower"]
     NO_BUILDING_CITIES = [
+        "09260",
         "11082",
         "25573",
         "26030",
